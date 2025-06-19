fca0c5bd725cb03c8ea8ef6f2d75544c74549012
Munish Kumar <40138449+ssd2658@users.noreply.github.com>
2025-06-19 23:03:59 +0530

    application starting

--- am-trade-models/src/main/java/am/trade/models/config/MongoConfig.java
+++ am-trade-models/src/main/java/am/trade/models/config/MongoConfig.java
@@ -1,11 +1,25 @@
 package am.trade.models.config;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+
+import jakarta.validation.constraints.NotNull;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
-import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import java.util.Collection;
+import java.util.Collections;
 
 /**
  * MongoDB configuration class
@@ -13,14 +27,44 @@ import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 @Configuration
 @EnableMongoRepositories(basePackages = "am.trade.models.repository")
 @EnableMongoAuditing
-public class MongoConfig {
+public class MongoConfig extends AbstractMongoClientConfiguration {
+
+    @Value("${spring.data.mongodb.uri:mongodb://localhost:27017/trade-management}")
+    private String mongoUri;
     
-    /**
-     * Configure validation for MongoDB documents
-     */
+    @Value("${spring.data.mongodb.database:trade-management}")
+    private String databaseName;
+
+    @Override
+    @NotNull
+    protected String getDatabaseName() {
+        return databaseName;
+    }
+
+    @Override
+    @NotNull
+    public MongoClient mongoClient() {
+        final ConnectionString connectionString = new ConnectionString(mongoUri);
+        final MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+        return MongoClients.create(mongoClientSettings);
+    }
+    
     @Bean
-    public ValidatingMongoEventListener validatingMongoEventListener(LocalValidatorFactoryBean factory) {
-        return new ValidatingMongoEventListener(factory);
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongoClient(), getDatabaseName());
+    }
+    
+    @Bean
+    public MongoTransactionManager transactionManager(MongoDatabaseFactory dbFactory) {
+        return new MongoTransactionManager(dbFactory);
+    }
+    
+    @Override
+    @NotNull
+    public Collection<String> getMappingBasePackages() {
+        return Collections.singleton("am.trade.models");
     }
     
     /**
