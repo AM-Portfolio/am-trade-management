a6794bbdc7ccc6a08b526fff8a1da3bdba13378e
Gyaneshwar kumar <kumargyan89@gmail.com>
2026-05-11 18:16:08 +0530

    Fix: Permitted actuator health sub-endpoints for Kubernetes probes

--- am-trade-api/src/main/java/am/trade/api/config/SecurityConfig.java
+++ am-trade-api/src/main/java/am/trade/api/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/actuator/health", "/actuator/info","/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**",
+                        .requestMatchers("/actuator/health/**", "/actuator/info", "/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**",
                                 "/api-docs/**")
                         .permitAll()
                         .anyRequest().authenticated())
